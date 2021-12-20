22f8529f5d393f25a813f9fa2ea0cedbbb087eb0
Khushbu Kella <88418675+Khushbukela@users.noreply.github.com>
2021-12-20 15:47:28 +0530

    Update sample-go-consumer.rb

--- Formula/sample-go-consumer.rb
+++ Formula/sample-go-consumer.rb
@@ -1,7 +1,7 @@
 class SampleGoConsumer < Formula
   homepage "https://hotstar.atlassian.net/wiki/spaces/HP2/pages/1516241986/Data+Platform+Onboarding"
   url "git@github.com:Khushbukela/sample-go-consumer.git",  using: :git, :branch => "main"
-  version '200.0.17'
+  version "200.0.17"
   sha256 "8127c1f9257a02ce47345e801b547060610d0b15e1e458110fba2aeeeb9eea03"
   def install
       bin.install "main"
